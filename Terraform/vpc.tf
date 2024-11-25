6bedbaaea44ff6c78a712a1a106e335b51f9a48f
Karan Patil <73995456+patilkaran1997@users.noreply.github.com>
2024-11-25 18:32:34 +0530

    Update vpc.tf

--- Terraform/vpc.tf
+++ Terraform/vpc.tf
@@ -1,3 +1,7 @@
+# Fetch Available Availability Zones
+data "aws_availability_zones" "available" {}
+
+# VPC Creation
 resource "aws_vpc" "main" {
   cidr_block = var.vpc_cidr
   enable_dns_support = true
@@ -7,25 +11,30 @@ resource "aws_vpc" "main" {
   }
 }
 
+# Public Subnets in Multiple AZs
 resource "aws_subnet" "public" {
   count = length(var.public_subnet_cidrs)
-  vpc_id            = aws_vpc.main.id
-  cidr_block        = var.public_subnet_cidrs[count.index]
-  map_public_ip_on_launch = true
+  vpc_id                    = aws_vpc.main.id
+  cidr_block                = var.public_subnet_cidrs[count.index]
+  availability_zone         = element(data.aws_availability_zones.available.names, count.index)
+  map_public_ip_on_launch   = true
   tags = {
     Name = "mern-public-subnet-${count.index}"
   }
 }
 
+# Private Subnets in Multiple AZs
 resource "aws_subnet" "private" {
   count = length(var.private_subnet_cidrs)
-  vpc_id     = aws_vpc.main.id
-  cidr_block = var.private_subnet_cidrs[count.index]
+  vpc_id                    = aws_vpc.main.id
+  cidr_block                = var.private_subnet_cidrs[count.index]
+  availability_zone         = element(data.aws_availability_zones.available.names, count.index)
   tags = {
     Name = "mern-private-subnet-${count.index}"
   }
 }
 
+# Internet Gateway
 resource "aws_internet_gateway" "main" {
   vpc_id = aws_vpc.main.id
   tags = {
@@ -33,6 +42,7 @@ resource "aws_internet_gateway" "main" {
   }
 }
 
+# Public Route Table
 resource "aws_route_table" "public" {
   vpc_id = aws_vpc.main.id
   tags = {
@@ -40,12 +50,14 @@ resource "aws_route_table" "public" {
   }
 }
 
+# Route for Public Subnet
 resource "aws_route" "public" {
   route_table_id         = aws_route_table.public.id
   destination_cidr_block = "0.0.0.0/0"
   gateway_id             = aws_internet_gateway.main.id
 }
 
+# Route Table Association for Public Subnets
 resource "aws_route_table_association" "public" {
   count          = length(var.public_subnet_cidrs)
   subnet_id      = aws_subnet.public[count.index].id
