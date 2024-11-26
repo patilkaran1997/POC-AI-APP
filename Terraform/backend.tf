2788d5d20ab4196fe69f1783f638f5390db70272
Karan Patil <73995456+patilkaran1997@users.noreply.github.com>
2024-11-26 11:12:12 +0530

    Update backend.tf

--- Terraform/backend.tf
+++ Terraform/backend.tf
@@ -1,10 +1,10 @@
 terraform {
   backend "s3" {
-    bucket = "your-terraform-state-bucket"
+    bucket = "my-bucket-ssemo"
     key    = "path/to/terraform.tfstate"
     region = "ap-south-1"
 
     # Optional: Use DynamoDB for state locking to prevent concurrent updates
-    dynamodb_table = "your-dynamodb-table"
+    dynamodb_table = "terraform-locks"
   }
 }
