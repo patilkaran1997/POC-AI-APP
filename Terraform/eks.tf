13aeaafa95c2a1870a777183325248f3af4139ed
Karan Patil <73995456+patilkaran1997@users.noreply.github.com>
2024-11-26 19:37:07 +0530

    Update eks.tf

--- Terraform/eks.tf
+++ Terraform/eks.tf
@@ -114,4 +114,6 @@ resource "aws_route_table_association" "public_route_table_assoc" {
   count          = length(aws_subnet.public)
   subnet_id      = aws_subnet.public[count.index].id
   route_table_id = aws_route_table.public_route_table.id
+  # Ensure Terraform manages the associations properly
+  depends_on = [aws_route_table.public_route_table]
 }
